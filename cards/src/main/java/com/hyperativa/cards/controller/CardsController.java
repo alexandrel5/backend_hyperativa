6fa28f69d82db6abc4ddd6c829923b605dda292c
Laptop Alexandre <alexandrecandidol5@gmail.com>
2026-02-11 16:30:10 -0300

    updated path

--- cards/src/main/java/com/hyperativa/cards/controller/CardsController.java
+++ cards/src/main/java/com/hyperativa/cards/controller/CardsController.java
@@ -4,23 +4,21 @@ import com.hyperativa.cards.constants.CardsConstants;
 import com.hyperativa.cards.dto.CardDto;
 import com.hyperativa.cards.dto.ResponseDto;
 import com.hyperativa.cards.service.ICardsService;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/api/v1", produces = {MediaType.APPLICATION_JSON_VALUE})
+@RequestMapping(path = "/api/card/v1", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
 public class CardsController {
 
     private ICardsService iCardsService;
 
-    @PostMapping("/card")
+    @PostMapping("/create")
     public ResponseEntity<ResponseDto> createCard(@RequestBody CardDto cardDto){
 
     iCardsService.createCard(cardDto);
@@ -29,5 +27,13 @@ public class CardsController {
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDto(CardsConstants.STATUS_201, CardsConstants.MESSAGE_201));
 
+    }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<CardDto> fetchCardDetails(@RequestParam
+                                                     @Pattern(regexp="(^$|[0-9]{10})",message = "Card number must be 10 digits")
+                                                     String mobileNumber) {
+        CardDto cardsDto = iCardsService.fetchCard(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
     }
 }
--- cards/src/main/java/com/hyperativa/cards/controller/UsersController.java
+++ cards/src/main/java/com/hyperativa/cards/controller/UsersController.java
@@ -14,13 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api/v1", produces = {MediaType.APPLICATION_JSON_VALUE})
+@RequestMapping(path = "/api/user/v1", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
 public class UsersController {
 
     private IUsersService iUsersService;
 
-    @PostMapping("/user")
+    @PostMapping("/create")
     public ResponseEntity<ResponseDto> createUser(@RequestBody UserDto userDto){
 
         iUsersService.createUser(userDto);
