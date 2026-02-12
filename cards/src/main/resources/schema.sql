32306bfe97bd441be76aa1bfa841dc06dceed3b8
Laptop Alexandre <alexandrecandidol5@gmail.com>
2026-02-12 16:23:01 -0300

    Updates card_number size

--- cards/src/main/resources/schema.sql
+++ cards/src/main/resources/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 CREATE TABLE IF NOT EXISTS `cards` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `user_id` INT NOT NULL,
-    `card_number` VARCHAR(16) NOT NULL UNIQUE, -- Change this to VARCHAR(19) for credit cards with spaces or dashes
+    `card_number` VARCHAR(19) NOT NULL UNIQUE, -- Change this to VARCHAR(19) for credit cards with spaces or dashes
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
     FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE
 );
