056064fb95f9f0000753a09070e88e788317b11c
Laptop Alexandre <alexandrecandidol5@gmail.com>
2026-02-18 22:03:59 -0300

    securityFilterChain

--- cards/src/main/java/com/hyperativa/cards/config/SecurityConfig.java
+++ cards/src/main/java/com/hyperativa/cards/config/SecurityConfig.java
@@ -2,13 +2,33 @@ package com.hyperativa.cards.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/**").authenticated()
+
+                        .anyRequest().permitAll()
+                )
+
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> {})  // or .jwt(jwtConfigurer -> ...) if customized
+                );
+
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
